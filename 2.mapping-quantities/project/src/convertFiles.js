bbb299452dd5c3b9d3085de939db94ded26c0c4e
Yiran Ni <yirann@uw.edu>
2019-12-03 19:42:48 -0500

    init project

--- 2.mapping-quantities/project/src/convertFiles.js
+++ 2.mapping-quantities/project/src/convertFiles.js
@@ -2,8 +2,8 @@ let csvToJson = require('convert-csv-to-json');
 let archiveInput = "../data/johnstons-archive.csv"
 let archiveOutput = '../data/johnstons-archive.json'
 
-let cancerrateInput = "../data/share-of-population-with-cancer.csv"
-let cancerrateOutput = "../data/share-of-population-with-cancer.json"
+let cancerrateInput = "../data/highlightIds.csv"
+let cancerrateOutput = "../data/highlightIds.json"
 
 // csvToJson.fieldDelimiter(',').generateJsonFileFromCsv(archiveInput, archiveOutput)
-csvToJson.fieldDelimiter(',').generateJsonFileFromCsv(cancerrateInput, cancerrateOutput)
+csvToJson.generateJsonFileFromCsv(cancerrateInput, cancerrateOutput)
